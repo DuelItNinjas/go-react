05124ea709547af9e2a2e4fd5c212172ee7f51e0
thinkty <thinkty@naver.com>
2022-01-24 13:23:24 +0900

    Added basic codes to serve static files from /web/dist directory

--- cmd/go-react/main.go
+++ cmd/go-react/main.go
@@ -1,8 +1,12 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/thinkty/go-react/internal/router"
+)
 
 func main() {
 	log.Print("Starting go-react...")
-
+	router.Start(9000, "./web/dist")
 }
